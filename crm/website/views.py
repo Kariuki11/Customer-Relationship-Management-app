0f66b1fbdf9704363ce82fe02a1cd5c343bf8929
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-23 19:01:20 +0300

    "Updated database, added register functionality, and modified navbar and URL configurations"

--- crm/website/views.py
+++ crm/website/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+
 def home(request):
     # Check to see if logging in 
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        
         # Authenticate the user
         user = authenticate(request, username=username, password=password)
+        
         if user is not None:
             login(request, user)
             messages.success(request, 'You have successfully logged in')
@@ -24,6 +27,73 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'website/register.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -59,3 +129,5 @@ def logout_user(request):
 
 
 # # Heading to Logout
+
+# Line 1 to line 26.
