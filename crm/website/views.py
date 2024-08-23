9216bd74b49ea8f19d048ef2a2f1b6268b348678
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-23 18:44:53 +0300

    "Updated navbar template to include login/logout links, modified views.py to handle login and logout functionality, and changed db.sqlite3 file."

--- crm/website/views.py
+++ crm/website/views.py
@@ -12,19 +12,50 @@ def home(request):
         if user is not None:
             login(request, user)
             messages.success(request, 'You have successfully logged in')
-            return redirect(request, 'website/home.html', {})
+            return redirect('home')  # Redirects to the 'home' URL pattern
         else:
-            messages.success(request, "There Was An Error Logging In, Please Try Again")
-            return redirect('home')
+            messages.error(request, "There was an error logging in, please try again")
+            return redirect('home')  # Redirects to the 'home' URL pattern
     else:
-        return render(request, 'website/home.html')
-        
-    return render(request, 'website/home.html', {})
-
+        return render(request, 'website/home.html')  # Render the template for GET requests
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('home')
 
 
-# Heading to Logout
+
+# from django.shortcuts import render, redirect
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib import messages
+
+# def home(request):
+#     # Check to see if logging in 
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         # Authenticate the user
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             messages.success(request, 'You have successfully logged in')
+#             return redirect(request, 'website/home.html', {})
+#         else:
+#             messages.success(request, "There Was An Error Logging In, Please Try Again")
+#             return redirect('home')
+#     else:
+#         return render(request, 'website/home.html')
+        
+#     return render(request, 'website/home.html', {})
+
+
+
+# def logout_user(request):
+#     logout(request)
+#     messages.success(request, "You have een logged out ...")
+#     return redirect('home')
+
+
+# # Heading to Logout
