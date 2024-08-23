99aea6b31cd4070d1a0990134d241deb01f0a0a4
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-23 09:05:37 +0300

    "Added imports for authenticate, login, logout, and messages from django.contrib.auth"

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'website/home.html', {})
