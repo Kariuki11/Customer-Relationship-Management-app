a8831c30402cf59b2e5c3ea4b7073482ede81add
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-23 19:17:00 +0300

    "Added SignUpForm class to forms.py, inheriting from UserCreationForm, with custom fields and widgets."

--- crm/website/forms.py
+++ crm/website/forms.py
@@ -1,1 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
+    
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
