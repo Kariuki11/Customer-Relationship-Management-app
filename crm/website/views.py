a40363d5c299dcd32799d9ed46b57c0757227390
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-23 20:16:50 +0300

    "Updated home.html and register.html templates, added SignUpForm to views.py and implemented user registration functionality"

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
@@ -29,7 +30,20 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'website/register.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate & Login.
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You have successfully registered')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'website/register.html', {'form':form})
 
 
 
