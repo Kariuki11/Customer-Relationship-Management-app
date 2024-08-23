0b0fc0345165eca871a9a6f2395e074fd33cae01
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-23 09:26:08 +0300

    "Updated home.html template, added login form, and modified urls.py and views.py files"

--- crm/website/views.py
+++ crm/website/views.py
@@ -4,3 +4,9 @@ from django.contrib import messages
 
 def home(request):
     return render(request, 'website/home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
