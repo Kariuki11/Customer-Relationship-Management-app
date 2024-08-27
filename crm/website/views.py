1cc9a3d293b07d312ee3809e4dde8b1fde540232
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-27 10:34:19 +0300

    "Updated home.html template to display records and modified views.py to pass records to the template"

--- crm/website/views.py
+++ crm/website/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
     # Check to see if logging in 
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        
         # Authenticate the user
         user = authenticate(request, username=username, password=password)
-        
         if user is not None:
             login(request, user)
             messages.success(request, 'You have successfully logged in')
@@ -21,7 +21,7 @@ def home(request):
             messages.error(request, "There was an error logging in, please try again")
             return redirect('home')  # Redirects to the 'home' URL pattern
     else:
-        return render(request, 'website/home.html')  # Render the template for GET requests
+        return render(request, 'website/home.html', {'records':records})  # Render the template for GET requests
 
 
 def logout_user(request):
