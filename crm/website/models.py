ce79374e2a58309f06fb0a01621741aa7a78262e
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-27 10:45:33 +0300

    Added 'country' field to Record model in models.py.

--- crm/website/models.py
+++ crm/website/models.py
@@ -11,6 +11,7 @@ class Record(models.Model):
     state = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=50)
     
     def __str__(self):
         return(f"{self.first_name} {self.last_name}")
