a9c548edb182446c7ef2bc85c921d82f27cf7a24
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-27 10:13:14 +0300

    "Fixed typo in model inheritance: 'model.Model' -> 'models.Model'"

--- crm/website/models.py
+++ crm/website/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Record(model.Model):
+class Record(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
