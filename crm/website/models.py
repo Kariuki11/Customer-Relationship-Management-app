d5edee289d96f25807a694e07aff0c26bf215eb1
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-27 10:06:43 +0300

    "Added fields to Record model: first_name, last_name, email, phone, address, city, state, and zip_code."

--- crm/website/models.py
+++ crm/website/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 class Record(model.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=20)
