7745035dfd3c3f3b2e68f53a552d21b813dce1be
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-27 09:56:51 +0300

    "Updated db.sqlite3, removed timestamp from register.html, and commented out login path in urls.py"

--- crm/website/urls.py
+++ crm/website/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('login/', views.login_user, name='login'),
+    #path('login/', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
     path('register', views.register_user, name='register'),
 ]
